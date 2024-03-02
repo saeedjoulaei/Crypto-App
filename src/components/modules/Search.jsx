07888c52ae047e195d31f14a22d67775063e18d4
saeed joulaei <saeedjoulaei86@gmail.com>
2024-03-03 01:39:20 +0330

    cleanup function and showing searched data in UI

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -1,12 +1,55 @@
+import { useEffect, useState } from "react";
+import { searchCoin } from "../../services/cryptoApi";
+
 function Search({ currency, setCurrency }) {
+  const [text, setText] = useState("");
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    setCoins([]);
+    if (!text) return;
+    const search = async () => {
+      try {
+        const res = await fetch(searchCoin(text), {
+          signal: controller.signal,
+        });
+        const json = await res.json();
+        console.log(json);
+        if (json.coins) {
+          setCoins(json.coins);
+        } else {
+          alert(json.status.error_message);
+        }
+      } catch (error) {
+        if (error.name !== "AbortError") alert(error.message);
+      }
+    };
+    search();
+    return () => controller.abort();
+  }, [text]);
   return (
     <div>
-      <input type="text" />
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
       <select onChange={(e) => setCurrency(e.target.value)} value={currency}>
         <option value="usd">USD</option>
         <option value="eur">EUR</option>
         <option value="jpy">JPY</option>
       </select>
+      <div>
+        <ul>
+          {coins.map((coin) => (
+            <li key={coin.id}>
+              <img src={coin.thumb} alt={coin.name} />
+              <p>{coin.name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -4,4 +4,6 @@ const API_KEY = "CG-4qWxwYjzfGivcefbsdtqzgxq";
 const getCoinList = (page, currency) => {
   return `${BASE_URL}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=10&page=${page}&locale=en&x_cg_demo_api_key=${API_KEY}`;
 };
-export { getCoinList };
+const searchCoin = (query) =>
+  `${BASE_URL}/search?query=${query}&locale=en&x_cg_demo_api_key=${API_KEY}`;
+export { getCoinList, searchCoin };
