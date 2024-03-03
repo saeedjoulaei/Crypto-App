79ea24d23b8c1987eaee1a314d6b9ae1b03c0580
saeed joulaei <saeedjoulaei86@gmail.com>
2024-03-04 02:08:20 +0330

    a bug in search section fixed

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -48,24 +48,26 @@ function Search({ currency, setCurrency }) {
         <option value="eur">EUR</option>
         <option value="jpy">JPY</option>
       </select>
-      <div className={styles.searchResult}>
-        {isLoading && (
-          <RotatingLines
-            width="50px"
-            height="50px"
-            strokeColor="#3874ff"
-            strokeWidth="2"
-          />
-        )}
-        <ul>
-          {coins.map((coin) => (
-            <li key={coin.id}>
-              <img src={coin.thumb} alt={coin.name} />
-              <p>{coin.name}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
+      {(!!coins.length || isLoading) && (
+        <div className={styles.searchResult}>
+          {isLoading && (
+            <RotatingLines
+              width="50px"
+              height="50px"
+              strokeColor="#3874ff"
+              strokeWidth="2"
+            />
+          )}
+          <ul>
+            {coins.map((coin) => (
+              <li key={coin.id}>
+                <img src={coin.thumb} alt={coin.name} />
+                <p>{coin.name}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
